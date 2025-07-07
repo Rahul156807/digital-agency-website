c9b8170cc5e8d532a17c30488dd436e40265f495
Rahul156807 <boss123@gmail.com>
2025-07-07 11:29:03 +0530

    Initial project structure setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes will be added here
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
