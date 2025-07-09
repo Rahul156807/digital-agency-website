862c4426726bee2781973d17d2ce4d97f769b451
Rahul156807 <boss123@gmail.com>
2025-07-09 17:07:11 +0530

    Updated services section with new item

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes will be added here
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+// Routes
+app.use("/api/contact", require("./routes/contactRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
